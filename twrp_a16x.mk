3c57e88103b94340d1b843a645f98abcb63331d2
kay6888 <kaynikko88@gmail.com>
2025-09-16 22:55:00 -0500

    Update twrp_a16x.mk

--- twrp_a16x.mk
+++ twrp_a16x.mk
@@ -9,6 +9,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from common tree
+$(call inherit-product, device/samsung/s5e8535-common/BoardConfigCommon.mk
+
+
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
