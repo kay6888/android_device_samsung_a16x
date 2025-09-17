6c8481e5f4e4c133ccfafab0ae50fe578214e0ce
kay6888 <kaynikko88@gmail.com>
2025-09-16 21:56:52 -0500

    Update twrp_a16x.mk

--- twrp_a16x.mk
+++ twrp_a16x.mk
@@ -18,12 +18,10 @@ $(call inherit-product, device/samsung/a16x/device.mk)
 PRODUCT_DEVICE := a16x
 PRODUCT_NAME := twrp_a16x
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A166E
+PRODUCT_MODEL := SM-A166U1
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a16xdxx-user 13 TP1A.220624.014 A166EXXU2AXJ2 release-keys"
-
-BUILD_FINGERPRINT := samsung/a16xdxx/a16x:13/TP1A.220624.014/A166EXXU2AXJ2:user/release-keys
+    
